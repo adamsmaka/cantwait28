ad2e88c4a93a7c1f132b87a2b387b8a18ece7101
Adrian Bugała <adbugala@gmail.com>
2022-01-17 15:42:37 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:cantwait28/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-//initialize
+//test
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
