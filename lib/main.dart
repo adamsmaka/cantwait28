be508760c3bd3dc1721e1a4361d868417263f4cb
Adam Smaka <adam@smaka.dev>
2022-03-11 12:51:25 +0100

    Refactor main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
+      theme: getTheme(),
       home: const AuthGate(),
+    );
+  }
+
+  ThemeData getTheme() {
+    return ThemeData(
+      primarySwatch: Colors.purple,
     );
   }
 }
