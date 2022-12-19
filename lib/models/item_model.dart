398d3ef82680d6c6350ad37c9a3224c1b3d550d6
Patryk Karpinski <patryk.karpinski@interia.pl>
2022-12-19 20:05:46 +0100

    implement item model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,12 @@
+class ItemModel {
+  ItemModel(
+      {required this.id,
+      required this.imageURL,
+      required this.releaseDate,
+      required this.title});
+
+  final String id;
+  final String imageURL;
+  final DateTime releaseDate;
+  final String title;
+}
