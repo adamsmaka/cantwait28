8c755d523658363d2adbdb37e204909c77f4c427
Michał Trzopek <michaell931@gmail.com>
2023-06-19 18:49:09 +0200

    Repair error logic, create login/register/logout

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,13 +1,21 @@
 class ItemModel {
-  ItemModel(
-      {required this.id,
-      required this.title,
-      required this.imageURL,
-      required this.releaseDate});
+  ItemModel({
+    required this.id,
+    required this.title,
+    required this.imageURL,
+    required this.relaseDate,
+  });
 
   final String id;
-
   final String title;
   final String imageURL;
-  final DateTime releaseDate;
+  final DateTime relaseDate;
+
+  String daysLeft() {
+    return relaseDate.difference(DateTime.now()).inDays.toString();
+  }
+
+  String relaseDateFormatted() {
+    return 'example';
+  }
 }
